cad99dc13b52f60e90be1e2b7e31534db21f84f3
Renard Bergson <renardrock@gmail.com>
2023-12-25 15:03:22 -0300

    initial structure + material UI

---
+++ src/app/theme.js
@@ -0,0 +1,41 @@
+'use client'
+
+import { createTheme } from '@mui/material'
+
+import { purple } from '@mui/material/colors'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: '#f44336',
+    },
+  },
+
+  spacing: 8, 
+  /* 
+    Em cada componente, o valor recebido no método theme.spacing() será multiplicado
+    pelo valor atribuido na propriedade "spacing", na configuração do tema. O valor 8 
+    já é o padrão fornecido pelo tema, MESMO QUE NEM ESCREVAMOS ESSA PROPRIEDADE!
+
+    Ex 1: 
+
+    const theme: createTheme({
+      spacing: 8 (padrão)
+    })
+
+    <span style={{margin: theme.spacing(2)}}> ... <span/> = 8 * 2 = 16px
+    
+    Ex 2:
+
+    const theme: createTheme({
+      spacing: 4 (padrão)
+    })
+
+    <span style={{margin: theme.spacing(2)}}> ... <span/> = 4 * 2 = 8px
+  */
+})
+
+export default theme
