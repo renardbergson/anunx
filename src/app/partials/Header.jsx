a04e43291aa4877e97bedadfd1786953d9624c07
Renard Bergson <renardrock@gmail.com>
2023-12-31 13:57:27 -0300

    inserting drawer menu

--- src/app/partials/Header.jsx
+++ src/app/partials/Header.jsx
@@ -1,32 +1,71 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import React, {useState} from 'react'
+import Link from 'next/link'
+
+import {
+  useTheme,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Avatar,
+  Menu,
+  MenuItem,
+  Divider,
+} from '@mui/material'
 
 export default function ButtonAppBar() {
+  const theme = useTheme()
+
+  const [anchorUserMenu, setAnchorUserMenu] = useState(false)
+
+  const userMenuIsOpen = Boolean(anchorUserMenu) 
+  // quando o objeto de "e.currentTarget" for recebido, será transformado em um booleano = true
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link href="/" style={{color: theme.palette.secondary.main, textDecoration: 'none'}}>
+            <Button color='secondary' size='large' sx={{letterSpacing: '2px', fontSize: '1em'}}>
+              Anunx
+            </Button>
+          </Link>
+        </Typography>
+
+        <Link href="/pages/user/publish" style={{color: theme.palette.secondary.main}}>
+          <Button color="inherit" variant='outlined' size='small'>
+            Anunciar e Vender
+          </Button>
+        </Link>
+
+        <IconButton sx={{marginLeft: theme.spacing(3)}} onClick={e => setAnchorUserMenu(e.currentTarget)}>
+          <Avatar src=""/>
+
+          <Typography variant="subtitle2" sx={{color: theme.palette.secondary.main, marginLeft: theme.spacing(1)}}>
+            Renard Bergson
           </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+        </IconButton>
+
+        <Menu 
+          open={userMenuIsOpen}
+          anchorEl={anchorUserMenu}
+          onClose={() => setAnchorUserMenu(null)}
+        >
+          <MenuItem>
+            <Link href='/pages/user/dashboard' style={{textDecoration: 'none', color: theme.palette.primary.main}}>
+              Meus anúncios  
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            Sobre nós
+          </MenuItem>
+          <Divider />
+          <MenuItem>
+            Sair
+          </MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  )
 }
