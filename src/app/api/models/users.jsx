1f9345575eea47ecabacd4c6fc2dfa1005f7cd46
Renard Bergson <renardrock@gmail.com>
2024-01-09 17:02:11 -0300

    api post users + password crypto

---
+++ src/app/api/models/users.jsx
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "o campo 'nome' é obrigatório"]
+  },
+  email: {
+    type: String,
+    required: [true, "o campo 'e-mail' é obrigatório"]
+  },
+  password: {
+    type: String,
+    required: [true, "o campo 'senha' é obrigatório"]
+  }
+})
+
+export default mongoose.models.users || mongoose.model('users', schema)
--- src/app/api/users/route.jsx
+++ src/app/api/users/route.jsx
@@ -1,10 +1,30 @@
-import dbConnect from "@/app/utils/dbConnect"
-import { NextRequest as req, NextResponse as res } from "next/server"
-
-await dbConnect() 
+import dbConnect from "@/app/api/utils/dbConnect"
+import { crypto } from "@/app/api/utils/password"
+import UsersModels from "../models/users"
+import { NextResponse as res } from "next/server"
 
 export async function GET () {
+  await dbConnect() 
+
   return res.json({
     message: 'lista de usuários!'
   })
+}
+
+export async function POST (req) {
+  const {name, email, password} = await req.json()
+  
+  await dbConnect()
+
+  const passwordCrypto = await crypto(password)
+
+  const user = new UsersModels({
+    name: name,
+    email: email,
+    password: passwordCrypto,
+  })
+
+  await user.save()
+
+  return res.json({ success: true }, {status: 201})
 }
